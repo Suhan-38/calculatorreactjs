d1d2c3d8ab37eb3402c3ac68508fbd123c5a51a0
Suhan-38 <125673689+Suhan-38@users.noreply.github.com>
2023-04-03 22:48:41 +0530

    Update App.js file

--- src/App.js
+++ src/App.js
@@ -5,24 +5,28 @@ import { useState } from "react"
 
 function App() {
 
-
+      // the above state is used to save the calculated value
       let [input, setInput] = useState("")
+      //it is to operate between two values
       function handleClick(value) {
 
             setInput(input + value)
 
       }
+      //it use eval to evaluate and then it adds to input
       function calculate(value) {
 
             let outputVal = eval(input)
             setInput(outputVal)
 
       }
+      // clears value  by setting it to empty string
       function handleClear() {
 
             setInput("")
 
       }
+      // we will send all this functions and state to keypad component and invoke it there
 
       return (
             <div className="container">
